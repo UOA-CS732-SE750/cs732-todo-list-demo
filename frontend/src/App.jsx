a49a98fb7ea8e8eeec3a9551510104cab1cd3353
Andrew Meads <andrew.meads@auckland.ac.nz>
2024-03-10 10:42:48 +1300

    Added code for step 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import { initialTodos } from "./initial-todos";
 
 function App() {
   return (
     <>
       <nav>
         <h1>My Todo List</h1>
+        <ul>
+          {initialTodos.map((todo) => (
+            <li key={todo._id}>{todo.description}</li>
+          ))}
+        </ul>
       </nav>
     </>
   );
