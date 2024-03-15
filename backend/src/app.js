48c255ce5d029246a9517e46eb081e435f0fa91e
Andrew Meads <andrew.meads@auckland.ac.nz>
2024-03-16 11:08:13 +1300

    Added step 9 code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,2 +1,18 @@
-// TODO Your code here.
-console.log("Hello World!");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello, world!" });
+});
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}!`));
